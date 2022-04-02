3bbc84ab27e4668d002a45d7ee1a4466f818ac4b
Domenico Monaco <monaco.d23@gmail.com>
2022-04-02 19:29:43 +0200

    update to Code v.0.0.3

--- starter-kit/html/src/index.js
+++ starter-kit/html/src/index.js
@@ -1,8 +1,23 @@
-import { vlad } from './main'
-import showInfo from './main'
+import cookiemodal from './components/cookiemodal.html';
+import fixedicon from './components/fixedicon.html';
+
 import './styles/main.scss'
 import './styles/style.css'
-import exampleImg from './assets/example.svg'
 
-import 'bootstrap';
-import 'jquery';
+$ = window.$ = window.jQuery = require('jquery');
+require('bootstrap');
+require('jquery');
+
+const classnotice = '.gdprcommons-notice';
+const classfixedicon = '.gdprcommons-fixedicon';
+
+const gdprcommonsnotice = $(classnotice).html(cookiemodal);
+const gdprcommonsfixedicon = $(classfixedicon).html(fixedicon);
+
+function showCookieAlert() {
+    $('#cookieModal').modal('show');
+}
+
+$('.gdprcommons-fixedicon .open-notice').on('click', function() {
+    showCookieAlert();
+});
