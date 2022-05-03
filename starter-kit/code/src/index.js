fd4bc7596ee476a2a16b00789f50755d57cd85cc
Domenico Monaco <monaco.d23@gmail.com>
2022-05-03 10:59:35 +0200

    aggiornamento

--- starter-kit/code/src/index.js
+++ starter-kit/code/src/index.js
@@ -137,24 +137,3 @@ function cookieAlert(config = null) {
         }
     }
 }
-
-const config = {
-    text: {
-        linkPrivacyPolicy: "http://www.gdprcommons.it",
-        textTitleModal: "Informativa Cookie",
-        textAbstractModal: "Questo sito utilizza esclusivamente cookie e/o altri strumenti di tracciamento di tipo tecnico o analitici al solo scopo di analisi statistica del traffico senza risalire all'identità del singolo utente. In oltre, non raccoglie informazioni personali in modo automatico senza esplicito consenso. In alcuni casi sono utilizzate risorse esterne. Ai fini legislativi, a scopo informativo si chiede la consultazione della Privacy Policy e proseguire cliccando su \'Ho capito\'",
-        textButtonAccept: "Ho capito",
-        textButtonOpenPolicy: "Leggi l\'informativa",
-        textButtonResetCookie: "elimina",
-    },
-
-    modal: {
-        showOnPageLoad: 'cookie', // ever | cookie
-    },
-
-    fixedicon: {
-        show: true
-    }
-};
-
-cookieAlert(config);
