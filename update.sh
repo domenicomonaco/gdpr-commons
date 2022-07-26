3a57bed0069ca8221eb5ca9e85f9e632920b1666
Domenico Monaco <monaco.d23@gmail.com>
2022-07-26 14:23:11 +0200

    update

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 param1=$1;
 
 git submodule foreach update --init
-git submodule foreach update;
+git submodule foreach update --force;
 git submodule foreach git pull --force; 
 git add . ; 
 git commit -m $param1; 
