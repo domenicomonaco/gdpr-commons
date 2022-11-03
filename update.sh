551b3795c9274d64422ccf4a3a2f67bf35b994fb
Domenico Monaco <82922+domenicomonaco@users.noreply.github.com>
2022-11-03 09:53:44 +0100

    update fixing

--- update.sh
+++ update.sh
@@ -4,8 +4,8 @@ param1=$1;
 rm -rf _webtoolkit;
 git clone https://github.com/domenicomonaco/gdpr-commons-web-toolkit.git _web_toolkit
 rm -rf _web_toolkit/.git
-git add . ; 
-git commit -m "update $param1"; 
+git add . ;
+git commit -m "update $param1";
 git fetch;
 git pull;
-git push;
+git push --force;
